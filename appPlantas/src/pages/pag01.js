571421b48baa57b7a08f30391b59b23f60bf03ed
Dev-plantas <aplicativoplantas@gmail.com>
2024-03-18 15:22:44 -0300

    atualização 004

--- appPlantas/src/pages/pag01.js
+++ appPlantas/src/pages/pag01.js
@@ -1,11 +1,21 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , SafeAreaView, ScrollView} from 'react-native';
 
 export default function Pag01() {
   return (
-    <View style={styles.container}>
-      <Text>pag01</Text>
-      
-    </View>
+    <ScrollView>
+      <SafeAreaView>
+         <View style={styles.container}>
+          <Text>pag01</Text>
+        </View>
+        <View style={{width: '100%', height: 250, backgroundColor:'red'}}/>
+        <View style={{width: '100%', height: 250, backgroundColor:'blue'}}/>
+        <View style={{width: '100%', height: 250, backgroundColor:'green'}}/>
+        <View style={{width: '100%', height: 250, backgroundColor:'orange'}}/>
+        <View style={{width: '100%', height: 250, backgroundColor:'black'}}/>
+      </SafeAreaView>
+     
+    </ScrollView>
+    
   );
 }
 
