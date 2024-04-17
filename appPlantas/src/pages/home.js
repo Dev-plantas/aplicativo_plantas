9b8ad7fc6761a616b1a9ec3fa1a726bbd915b324
gomesDaniel-dev <alex12xande@gmail.com>
2024-04-17 16:41:07 -0300

    envio

--- appPlantas/src/pages/home.js
+++ appPlantas/src/pages/home.js
@@ -1,9 +1,16 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function Home() {
   return (
     <View style={styles.container}>
       <Text>home</Text>
+  
+      <a href='./pag02.js'>
+        <Button
+        />
+      </a>
+   
+      
   
     </View>
   );
